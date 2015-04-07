4cf5336cfde378502d8eed6d0377d08325c38e92
Daniel Yang <danyang@nat-oitwireless-inside-vapornet100-c-21093.Princeton.EDU>
2015-04-07 18:23:16 -0400

    google map view added

---
+++ iOS/P4P/P4P/GoogleMapsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GoogleMapsViewController.swift
+//  P4P
+//
+//  Created by Daniel Yang on 4/7/15.
+//  Copyright (c) 2015 P4P. All rights reserved.
+//
+
+import UIKit
+
+class GoogleMapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
