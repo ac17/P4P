07e58b2f2d0238adba6e9f3d0c77f30d1dffe0e6
Daniel Yang <danyang@nat-oitwireless-inside-vapornet100-c-21093.Princeton.EDU>
2015-04-08 21:10:25 -0400

    track location

--- iOS/P4P/P4P/GoogleMapsViewController.swift
+++ iOS/P4P/P4P/GoogleMapsViewController.swift
@@ -8,23 +8,53 @@
 
 import UIKit
 
-class GoogleMapsViewController: UIViewController {
+class GoogleMapsViewController: UIViewController, CLLocationManagerDelegate {
+
+    var mapView: GMSMapView!
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-            longitude: 151.20, zoom: 6)
-        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
+        
+        var camera = GMSCameraPosition.cameraWithLatitude(40.348,
+            longitude: -74.653, zoom: 17)
+        mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
         self.view = mapView
         
+        // request access to user location
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+
+        // padding - need to find a better way of doing this than hardcoding
+        var mapInsets = UIEdgeInsetsMake(self.topLayoutGuide.length, 0.0, 50.0, 0.0)
+        mapView.padding = mapInsets
+        
+        /*
         var marker = GMSMarker()
         marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
+        */
+    }
+
+    // function called when authorization revoked or granted
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways {
+            locationManager.startUpdatingLocation()
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    
+    // function called when new location data received
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location = locations.first as? CLLocation {
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            locationManager.stopUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
