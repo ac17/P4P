241f28c9e4c022a5b15d17aa1022f31778fe36ad
vibhaa <vibhaa@princeton.edu>
2015-04-12 14:58:54 -0400

    chat history saving version 1

---
+++ php/chatClient/post1.php
@@ -0,0 +1,22 @@
+<?
+require_once('../database_connect.php');
+session_start();
+
+if(isset($_SESSION['user'])){
+    $text = $_POST['text'];
+     
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['user']['firstName']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+    fclose($fp);
+
+    $date =.date("g:i A").; // using date temporarily
+    $from = $_SESSION['user']['firstName'];
+    $to = $_SESSION['user']['firstName']; // dummy value
+    $saveresult = saveChat($from, $to, $date, $chat);
+
+    $insertq = "INSERT INTO chat_history (User_From, User_To, Time, Conversation) VALUES ('".$from."', '".$to."', '".$date."', '".$text."');";
+	$result = mysql_query($insertQ);
+	if (!$results) {
+		echo mysql_errno($connection) . ": " . mysql_error($connection) . "\n";
+	}
+?>
