cf9d103f3f1479a5893cde9414cfd36fa19b7d18
ac17 <angelicachen@nat-oitwireless-inside-vapornet100-b-25873.princeton.edu>
2015-04-14 16:38:43 -0400

    added a function for checking if the user is logged in

--- php/database_connect.php
+++ php/database_connect.php
@@ -1,9 +1,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <?php
+// start session
+session_start();
 //Get login information 
 require("database_login_info.php");
 //Create a generic error message
-$error = "An error occoured. Please refresh this page.";
+$error = "An error occurred. Please refresh this page.";
 //Connect to to the server 
 $connection = @mysql_connect($database_host,$database_username,$database_password) or die($error);
 //Select database
@@ -11,5 +13,16 @@ $database_select = @mysql_select_db($database_databaseName) or die($error);
 mysql_query("SET NAMES 'utf8'");
 mysql_query("SET CHARACTER SET utf8");
 mysql_query("SET COLLATION_CONNECTION = 'utf8_unicode_ci'");
+
+// Checks if the current user is logged in and returns TRUE or FALSE accordingly. If yes, then store the user's info in $_SESSION['user'].
+function isUserLoggedIn() {
+	$query = 'SELECT * FROM Users WHERE session_id="' . session_id() . '" LIMIT 1;';
+	$result = mysql_query($query);
+	if (mysql_num_rows($result) == 1) {
+		$_SESSION['user'] = mysql_fetch_assoc($result);
+		return TRUE;
+	}
+	return FALSE;
+}
 ?>
 </html>
