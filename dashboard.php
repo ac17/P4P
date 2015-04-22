9fc7da143c3a31ddce382bae187eacff66c92fee
ac17 <angelicachen@nat-oitwireless-inside-vapornet100-b-25127.princeton.edu>
2015-04-22 19:25:35 -0400

    trying to fix dashboard CSS problems

--- dashboard.php
+++ dashboard.php
@@ -28,11 +28,11 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <!-- Custom -->
-    <link rel="stylesheet" href="css/global.css">
     <link href="css/dashboard.css" rel="stylesheet">
     <link href="css/map.css" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/chat_style.css" />
     <link href="css/exchangeManager.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/global.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
