35432af8a03a3b8a90344b91c19ac954f6f98c7f
Frank Jiang <ffjiang@princeton.edu>
2015-04-22 21:04:03 -0400

    added current exchanges

---
+++ iOS/P4P/P4P/TabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarViewController.swift
+//  P4P
+//
+//  Created by Frank Jiang on 22/4/15.
+//  Copyright (c) 2015 P4P. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+        // Do any additional setup after loading the view.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
