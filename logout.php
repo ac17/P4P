301ec3d2e4204778d3cb092c306da672e44b3266
ac17 <angelicachen@nat-oitwireless-inside-vapornet100-a-27084.princeton.edu>
2015-05-12 12:39:36 -0400

    more comments

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 	 /* Connect to the database. */
 	 include_once('php/database_connect.php');
 
-	 /* Identify who is currently logged in and log out. */
+	 /* Identify who is currently logged in. Log out by setting session ID to NULL. */
 	 if (isUserLoggedIn()) {
 	 	$logoutQ = 'UPDATE Users SET session_id="NULL" WHERE netId="' . $_SESSION['user']['netId'] . '";';
 	 	mysql_query($logoutQ);
